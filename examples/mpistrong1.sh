6cd361285fbd40f591948b3bc5af93f4ce0a1e3a
david <david@darkstar.site>
2015-03-16 21:30:26 -0600

    added scripts

---
+++ examples/mpistrong1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -o strong1.out
+#PBS -e strong1.out
+#PBS -d ~/testCode
+#PBS -q micdev.q
+#PBS -V
+#PBS -l nodes=8
+# request 4 hours and 30 minutes of cpu time -l cput=04:30:00 
+# mail is sent to you when the job starts and when it terminates or aborts
+#PBS -m bea
+# specify your email address
+#PBS -M ddpruitt@miners.utep.edu
+
+# Simple strong scaling study with eam potential and 256,000 atoms
+# Simple strong scaling study with eam potential and 256,000 atoms
+mpirun -np 1  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
+
---
+++ examples/mpistrong32.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#PBS -o strong32.out
+#PBS -e strong32.out
+#PBS -d ~/testCode
+#PBS -q micdev.q
+#PBS -V
+#PBS -l nodes=8
+# request 4 hours and 30 minutes of cpu time -l cput=04:30:00 
+# mail is sent to you when the job starts and when it terminates or aborts
+#PBS -m bea
+# specify your email address
+#PBS -M ddpruitt@miners.utep.edu
+
+# Simple strong scaling study with eam potential and 256,000 atoms
+mpirun -np 32 ../bin/CoMD-mpi -e -i 4 -j 4 -k 2 -x 40 -y 40 -z 40
