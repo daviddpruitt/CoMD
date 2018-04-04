79c83ed899950e61aa8a5278bc2a416da53a98fe
David Pruitt <ddpruitt@miners.utep.edu>
2018-04-04 12:39:30 -0600

    Update mpi-weakScaling.sh

--- examples/mpi-weakScaling.sh
+++ examples/mpi-weakScaling.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# 5 runs at powerof 2 processes
+
 # Simple weak scaling study with eam potential and 32000 atoms per task
 mpirun -np 1  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 20 -y 20 -z 20
 mpirun -np 2  ../bin/CoMD-mpi -e -i 2 -j 1 -k 1 -x 40 -y 20 -z 20
