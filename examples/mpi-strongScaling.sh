d6dba5028240548b5666ce73dbe64f3df6013c67
David Pruitt <daviddpruitt@gmail.com>
2018-04-04 12:36:34 -0600

    Example of commiting

--- examples/mpi-strongScaling.sh
+++ examples/mpi-strongScaling.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# 5 runs
 # Simple strong scaling study with eam potential and 256,000 atoms
 mpirun -np 1  ../bin/CoMD-mpi -e -i 1 -j 1 -k 1 -x 40 -y 40 -z 40
 mpirun -np 2  ../bin/CoMD-mpi -e -i 2 -j 1 -k 1 -x 40 -y 40 -z 40
